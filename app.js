f7ff4988b36a8864316d8770b5508e5b17918682
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-01-04 15:34:19 +0200

    install express

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/employee/:id", (req, res) => {
+    console.log(req.params);
+    res.send({"message": "Employee retrieved"});
+});
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
